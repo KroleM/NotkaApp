6c9b85b352c2e29d6cdef22e06969b2d006d1732
Karol Marciniak <marciniakarol@gmail.com>
2023-11-22 16:31:42 +0100

    Add MAUI project

---
+++ NotkaMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotkaMobile
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ NotkaMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotkaMobile
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
