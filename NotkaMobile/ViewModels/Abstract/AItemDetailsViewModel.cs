f66affb80ccfc201d0c6c86075b9bb77f7169050
Karol Marciniak <marciniakarol@gmail.com>
2023-11-26 19:09:08 +0100

    Add abstract view models

---
+++ NotkaMobile/ViewModels/Abstract/AItemDetailsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using NotkaMobile.Services.Abstract;
+using System.Diagnostics;
+
+namespace NotkaMobile.ViewModels.Abstract
+{
+	[QueryProperty(nameof(ItemId), nameof(ItemId))]
+	public abstract class AItemDetailsViewModel<T> : BaseViewModel
+	{
+		protected AItemDetailsViewModel()
+		{
+			CancelCommand = new Command(OnCancel);
+			DeleteCommand = new Command(OnDelete);
+		}
+
+		public IDataStore<T> DataStore => DependencyService.Get<IDataStore<T>>();
+
+		public Command DeleteCommand { get; }
+		public Command CancelCommand { get; }
+		public abstract void LoadProperties(T item);
+		private async void OnDelete()	//FIXME przerobić na Task i dodać atrybut [RelayCommand]?
+		{
+			await DataStore.DeleteItemAsync(itemId);
+			// This will pop the current page off the navigation stack
+			await Shell.Current.GoToAsync("..");
+		}
+
+		private async void OnCancel()   //FIXME przerobić na Task i dodać atrybut [RelayCommand]?
+		{
+			// This will pop the current page off the navigation stack
+			await Shell.Current.GoToAsync("..");
+		}
+
+
+		private int itemId;
+		public int ItemId
+		{
+			get
+			{
+				return itemId;
+			}
+			set
+			{
+				itemId = value;
+				LoadItemId(value);
+			}
+		}
+
+		public async void LoadItemId(int itemId)
+		{
+			try
+			{
+				var item = await DataStore.GetItemAsync(itemId);
+				LoadProperties(item);
+			}
+			catch (Exception)
+			{
+				Debug.WriteLine("Failed to Load Item");
+			}
+		}
+	}
+}
--- NotkaMobile/ViewModels/Abstract/BaseViewModel.cs
+++ NotkaMobile/ViewModels/Abstract/BaseViewModel.cs
@@ -1,26 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NotkaMobile.ViewModels.Abstract
 {
-    public class BaseViewModel : ObservableObject
+    public abstract partial class BaseViewModel : ObservableObject
     {
+		[ObservableProperty]
 		bool isBusy = false;
-		public bool IsBusy
-		{
-			get { return isBusy; }
-			set { SetProperty(ref isBusy, value); }
-		}
 
+		[ObservableProperty]
 		string title = string.Empty;
-		public string Title
-		{
-			get { return title; }
-			set { SetProperty(ref title, value); }
-		}
 	}
 }
