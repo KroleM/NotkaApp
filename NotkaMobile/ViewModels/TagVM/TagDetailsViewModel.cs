1cc2949556aef5790affd8ff6afca0fe0b60db21
Karol Marciniak <marciniakarol@gmail.com>
2024-01-27 21:04:16 +0100

    Show list of tag related Notes
    * navigation to tapped note details

--- NotkaMobile/ViewModels/TagVM/TagDetailsViewModel.cs
+++ NotkaMobile/ViewModels/TagVM/TagDetailsViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using NotkaMobile.Service.Reference;
 using NotkaMobile.Services;
 using NotkaMobile.ViewModels.Abstract;
+using NotkaMobile.ViewModels.NoteVM;
+using NotkaMobile.Views.Notes.Note;
 using NotkaMobile.Views.Notes.Tag;
 
 namespace NotkaMobile.ViewModels.TagVM
@@ -28,11 +31,20 @@ namespace NotkaMobile.ViewModels.TagVM
 			Description = item.Description;
 			CreatedDate = item.CreatedDate;
 			ModifiedDate = item.ModifiedDate;
-			NotesForView = item.NotesForView;
+			NotesForView = item.NotesForView;	//dopisywanie w foreach?
 		}
 		protected async override void OnEdit()
 		{
 			await Shell.Current.GoToAsync($"{nameof(TagEditPage)}?{nameof(TagEditViewModel.ItemId)}={ItemId}");
+		}
+		[RelayCommand]
+		async System.Threading.Tasks.Task NoteTapped(NoteForView item)
+		{
+			if (item == null)
+			{
+				return;
+			}
+			await Shell.Current.GoToAsync($"{nameof(NoteDetailsPage)}?{nameof(NoteDetailsViewModel.ItemId)}={item.Id}");
 		}
 	}
 }
